45e0de2d093df104fdfc014ab0f5978f6ba91c88
yoshoku <yoshoku@outlook.com>
2020-11-03 17:53:44 +0900

    :articulated_lorry: Change spec file organization

---
+++ spec/driver2_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Lbfgsb do
+  describe 'driver2' do
+    it { expect('Hello, world').to start_with('Hello') }
+  end
+end
---
+++ spec/driver3_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Lbfgsb do
+  describe 'driver3' do
+    it { expect('Hello, world').to start_with('Hello') }
+  end
+end
