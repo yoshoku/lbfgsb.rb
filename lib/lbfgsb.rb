28f8102838dd429dd7252df593181ad4d3be3782
yoshoku <yoshoku@outlook.com>
2020-11-08 09:03:25 +0900

    :books: Add description about return value of minimize method

--- lib/lbfgsb.rb
+++ lib/lbfgsb.rb
@@ -18,17 +18,26 @@ module Lbfgsb
   #   \[lower, upper\] bounds for each element x. If nil is given, x is unbounded.
   # @param factr [Float] The iteration will be stop when
   #
-  #   `(f^k - f^\{k+1\})/max{|f^k|,|f^\{k+1\}|,1} <= factr * Lbfgsb::DBL_EPSILON`
+  #   (f^k - f^\{k+1\})/max{|f^k|,|f^\{k+1\}|,1} <= factr * Lbfgsb::DBL_EPSILON
   #
   #   Typical values for factr: 1e12 for low accuracy; 1e7 for moderate accuracy; 1e1 for extremely high accuracy.
   # @param pgtol [Float] The iteration will be stop when
   #
-  #   `max{|pg_i| i = 1, ..., n} <= pgtol`
+  #   max{|pg_i| i = 1, ..., n} <= pgtol
   #
   #   where pg_i is the ith component of the projected gradient.
   # @param maxcor [Integer] The maximum number of variable metric corrections used to define the limited memory matrix.
   # @param maxiter [Integer] The maximum number of iterations.
   # @param verbose [Integer/Nil] If negative value or nil is given, no display output is generated.
+  # @return [Hash] Optimization results; { x:, n_fev:, n_jev:, n_iter:, fnc:, jcb:, task:, success: }
+  #   - x [Numo::DFloat] Updated vector by optimization.
+  #   - n_fev [Interger] Number of calls of the objective function.
+  #   - n_jev [Integer] Number of calls of the jacobian.
+  #   - n_iter [Integer] Number of iterations.
+  #   - fnc [Float] Value of the objective function.
+  #   - jcb [Numo::Narray] Values of the jacobian
+  #   - task [String] Description of the cause of the termination.
+  #   - success [Boolean] Whether or not the optimization exited successfully.
   def minimize(fnc:, x_init:, jcb:, args: nil, bounds: nil, factr: 1e7, pgtol: 1e-5, maxcor: 10, maxiter: 15_000, verbose: nil)
     n_elements = x_init.size
     l = Numo::DFloat.zeros(n_elements)
